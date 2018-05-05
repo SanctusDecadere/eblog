d31d36a678938212af8cffad15b70e8949dbeb0c
sanctusdecadere <emiliosoto4@yahoo.com>
2018-05-05 13:44:33 -0400

    Added edit and delete functionality to posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,21 +1,20 @@
 # require 'pluralize'
 
 class PostsController < ApplicationController
+	before_action :post_params, only: [:create, :update]
+	before_action :post_id, only: [:show, :edit, :update, :destroy]
 
 	def index
 		@posts = Post.all.newest_first
 		# @posts = Post.all.order('created_at DESC')
 	end
 
-
-
 	def new
 		@post = Post.new
 	end
 
-
 	def create
-		@post = Post.new(post_params)
+		# @post = Post.new(post_params)
 		if @post.save
 			redirect_to @post
 		else
@@ -23,21 +22,41 @@ class PostsController < ApplicationController
 		end
 	end
 
-
 	def show
-		@post = Post.find(params[:id])
+		# @post = Post.find(params[:id])
+	end
 
+	def edit
+		# @post = Post.find(params[:id])
 		
 	end
 
+	def update
+		# @post = Post.find(params[:id])
+
+		if @post.update(post_params)
+			redirect_to @post
+		else
+			render 'edit'
+		end
+	end
 
 
+	def destroy
+		# @post = Post.find(params[:id])
+		@post.destroy
 
+		redirect_to root_path
+	end
 
 	private 
 
 		def post_params
-			params.require(:post).permit(:title, :body)
+			@post = params.require(:post).permit(:title, :body)
+		end
+
+		def post_id
+			@post = Post.find(params[:id])
 		end
 
 end
