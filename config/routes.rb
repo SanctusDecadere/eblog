2917bcb07efa5cabd6d0651173a3ddd516d15821
sanctusdecadere <emiliosoto4@yahoo.com>
2018-05-07 13:22:55 -0400

    Added sign in and logout path

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,18 @@
 Rails.application.routes.draw do
 
-  devise_for :users
+	root 'posts#index'
+
+  	devise_for :users
+	# devise_for :users do
+	#   get '/users/sign_out' => 'devise/sessions#destroy'
+	# end
+  
+
 	resources :posts do 
 		resources :comments
 	end
 
-	root 'posts#index'
+	
 
 	get '/about', to: 'pages#about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
