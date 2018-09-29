34415d648fa49e517dcebeecd738fccfa341652d
sanctusdecadere <emiliosoto4@yahoo.com>
2018-09-29 02:46:10 -0400

    wrote some js for toggling navbar on and off but commented it out for now

---
+++ app/assets/javascripts/nav-bar.js
@@ -0,0 +1,38 @@
+
+$('document').ready(function() {
+	console.log('ready to go');
+
+
+	// document.getElementById('navButton').addEventListener('click', function() {
+	// 	// $('#sidebar').css('display', 'block');
+	// 	$('#sidebar').toggle(function() {
+	// 		if($('#sidebar').display == 'none') {
+	// 			$('#sidebar').css('display', 'block');
+	// 		} else {
+	// 			$('#sidebar').css('display', 'none');
+	// 		}
+	// 		event.preventDefault();
+			
+	// 	})
+	// })
+
+
+// **************************************************************
+//This works but I want a fade in and out.
+// var toggle  = document.getElementById("navButton");
+// var content = document.getElementById("sidebar");
+
+// toggle.addEventListener("click", function() {
+//   content.style.display = (content.dataset.toggled ^= 1) ? "block" : "none";
+// });
+// **************************************************************
+
+
+
+
+
+
+})
+
+
+
